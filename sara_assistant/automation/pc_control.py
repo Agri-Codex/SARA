6684b521e67dd091ee8593d0fbfb3dfff2ea3aee
Agri-Codex <264327866+Agri-Codex@users.noreply.github.com>
2026-05-12 05:30:58 +0500

    Upgrade PC control for Phase 3

--- sara_assistant/automation/pc_control.py
+++ sara_assistant/automation/pc_control.py
@@ -1,6 +1,7 @@
 import os
 import webbrowser
 
+
 class PCControl:
     @staticmethod
     def open_google():
@@ -13,6 +14,20 @@ class PCControl:
         return "Opening YouTube"
 
     @staticmethod
+    def open_whatsapp_web():
+        webbrowser.open("https://web.whatsapp.com")
+        return "Opening WhatsApp Web"
+
+    @staticmethod
+    def open_notepad():
+        os.system("start notepad")
+        return "Opening Notepad"
+
+    @staticmethod
     def shutdown_pc():
+        return "Shutdown blocked until you say: confirm shutdown"
+
+    @staticmethod
+    def confirm_shutdown():
         os.system("shutdown /s /t 5")
-        return "Shutting down PC in 5 seconds"
+        return "Confirmed. Shutting down PC in 5 seconds"
